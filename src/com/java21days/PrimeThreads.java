3932f11c00c3fb8b4cfbdfbe9ff5ab1efe6fc876
Alejandro Salas <asalas.wd@outlook.com>
2022-11-01 08:00:01 -0400

    Create loop to check for prime and catch formatting error

--- src/com/java21days/PrimeThreads.java
+++ src/com/java21days/PrimeThreads.java
@@ -6,6 +6,15 @@ public class PrimeThreads {
     }
 
     public PrimeThreads(String[] args){
-
+        PrimeFinder[] finder = new PrimeFinder[args.length];
+        for(int i=0; i<args.length; i++){
+            try{
+                long count = Long.parseLong(args[i]);
+                finder[i] = new PrimeFinder(count);
+                System.out.println("Looking for primer " + count);
+            } catch (NumberFormatException nfe) {
+                System.out.println("Error: " + nfe.getMessage());
+            }
+        }
     }
 }
