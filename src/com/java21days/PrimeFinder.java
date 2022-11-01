4eb498026b5ddbd69c89b8a7a6c0f729b4536a4e
Alejandro Salas <asalas.wd@outlook.com>
2022-10-31 22:44:01 -0400

    Instantiate variables

--- src/com/java21days/PrimeFinder.java
+++ src/com/java21days/PrimeFinder.java
@@ -1,6 +1,10 @@
 package com.java21days;
 
 public class PrimeFinder implements Runnable {
+    public long target;
+    public long prime;
+    public boolean finished = false;
+    private Thread runner;
 
     @Override
     public void run() {
