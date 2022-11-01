e29a90f0fd9e95a6e9542e8e6fff9366cef564ec
Alejandro Salas <asalas.wd@outlook.com>
2022-11-01 07:57:14 -0400

    Create PrimeThreads class

--- src/com/java21days/PrimeThreads.java
+++ src/com/java21days/PrimeThreads.java
@@ -4,4 +4,8 @@ public class PrimeThreads {
     public static void main(String[] args) {
         PrimeThreads pt = new PrimeThreads(args);
     }
+
+    public PrimeThreads(String[] args){
+
+    }
 }
