eac3558aae2e0b4596e98c10586d53c37797034e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-01 08:06:29 -0400

    Complete Thread class

--- src/com/java21days/PrimeThreads.java
+++ src/com/java21days/PrimeThreads.java
@@ -15,6 +15,29 @@ public class PrimeThreads {
             } catch (NumberFormatException nfe) {
                 System.out.println("Error: " + nfe.getMessage());
             }
+            boolean complete = false;
+            while(!complete){
+                complete = true;
+                for (int j=0; j<finder.length; j++){
+                    if (finder[j] == null) continue;
+                    if(!finder[j].finished) {
+                        complete = false;
+                    } else {
+                        displayResult(finder[j]);
+                        finder[j] = null;
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ie){
+//                  Do nothing
+                }
+            }
         }
     }
+
+    private void displayResult(PrimeFinder finder){
+        System.out.println("Prime " + finder.target + " is " + finder.prime);
+    }
+
 }
